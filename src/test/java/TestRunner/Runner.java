5f50e998be52397458a9f594b0fd1458296aa879
Lionel M <lionel.mlb@gmail.com>
2020-10-23 13:34:02 +0200

    base project

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -1,14 +1,11 @@
-package TestRunner;		
+package testRunner;
 
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;		
+import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)				
-@CucumberOptions(features="Features",glue={"StepDefinition"})						
-public class Runner 	
-
-{		
-
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features", glue="stepDefinition")
+public class Runner {
 }
